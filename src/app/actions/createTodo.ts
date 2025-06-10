095025654a2fd3dc202881ca9ff7d174e4be5e87
Luka Talakvadze <lukatalakvadze@gmail.com>
2025-06-10 17:25:06 +0400

    feat: add unique user_id column in todos table so each user have their own todos displayed

--- src/app/actions/createTodo.ts
+++ src/app/actions/createTodo.ts
@@ -3,23 +3,42 @@
 import { createClient } from "../../../services/supabase/server";
 
 export async function createTodo(formData: FormData) {
+  const supabase = await createClient();
+
   const title = formData.get("title")!;
   const description = formData.get("description")!;
   const priority = formData.get("priority")!;
   const category = formData.get("category")!;
   const dueDate = formData.get("dueDate")!;
 
-  const supabase = await createClient();
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
 
-  const { error } = await supabase.from("todos").insert({
-    title,
-    description,
-    priority,
-    category,
-    due_date: dueDate || null,
-  });
+  if (user) {
+    const { data, error } = await supabase
+      .from("todos")
+      .insert([
+        {
+          title,
+          description,
+          priority,
+          category,
+          due_date: dueDate || null,
+          user_id: user.id,
+        },
+      ])
+      .select();
 
-  if (error) {
-    throw new Error(error.message);
+    if (error) {
+      console.error("Error adding todo:", error);
+      throw new Error("Failed to add todo: " + error.message);
+    } else {
+      console.log("Todo added successfully:", data);
+      return data;
+    }
+  } else {
+    console.warn("No authenticated user found for this action.");
+    throw new Error("Authentication required to create a todo.");
   }
 }
